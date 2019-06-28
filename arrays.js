c59eafc3c1202e9960dd6f28fa0146c882453071
Sadaf Farahmandifard <sadaffarahmandi@yahoo.com>
2019-06-28 00:02:54 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,5 +3,4 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'
 function addElementToBeginningOfArray(array, element){
   return [element, ...array];
 }
-function destructivelyAddElementToBeginningOfArray (array, element);
-return [destructivelyAddElementToBeginningOfArray.push("element")]
+function destructivelyAddElementToBeginningOfArray (array, element) {
