4b78d09b1f49e20f3f6111de95a48bf61279d90b
Sadaf Farahmandifard <sadaffarahmandi@yahoo.com>
2019-06-28 00:04:46 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -5,5 +5,8 @@ function addElementToBeginningOfArray(array, element){
 }
 function destructivelyAddElementToBeginningOfArray (array, element) {
   array.unshift(element);
-  return array
+  return array;
+}
+function addElementToEndOfArray (array, element){
+  
 }
